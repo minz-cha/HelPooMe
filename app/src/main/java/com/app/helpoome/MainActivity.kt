93fa62fb7aeba345f4f0f66addccffac02e65873
minju kim <demin1026@kpu.ac.kr>
2022-05-25 16:17:50 +0900

    구글지도 API 연동

--- app/src/main/java/com/app/helpoome/MainActivity.kt
+++ app/src/main/java/com/app/helpoome/MainActivity.kt
@@ -1,11 +1,131 @@
 package com.app.helpoome
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLng
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    val PERMISSIONS = arrayOf(
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    )
+
+    val REQUEST_PERMISSION_CODE = 1
+    val DEFAULT_ZOOM_LEVEL = 17f
+    val CITY_HALL = LatLng(37.5662952, 126.97794509999994)
+    var googleMap: GoogleMap? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mapView.onCreate(savedInstanceState)
+
+        if (checkPermissions()) {
+            initMap()
+        } else {
+            ActivityCompat.requestPermissions(this, PERMISSIONS, REQUEST_PERMISSION_CODE)
+        }
+
+        myLocationButton.setOnClickListener { onMyLocationButtonClick() }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray,
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        initMap()
+    }
+
+    private fun checkPermissions(): Boolean {
+
+        for (permission in PERMISSIONS) {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    permission
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                return false
+            }
+        }
+        return true
+    }
+
+    @SuppressLint("MissingPermission")
+    fun initMap() {
+        mapView.getMapAsync {
+
+            googleMap = it
+            it.uiSettings.isMyLocationButtonEnabled = false
+
+            when {
+                checkPermissions() -> {
+                    it.isMyLocationEnabled = true
+                    it.moveCamera(
+                        CameraUpdateFactory.newLatLngZoom(
+                            getMyLocation(),
+                            DEFAULT_ZOOM_LEVEL
+                        )
+                    )
+                }
+                else -> {
+                    it.moveCamera(CameraUpdateFactory.newLatLngZoom(CITY_HALL, DEFAULT_ZOOM_LEVEL))
+                }
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    fun getMyLocation(): LatLng {
+
+        val locationProvider: String = LocationManager.GPS_PROVIDER
+        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val lastKnownLocation: Location = locationManager.getLastKnownLocation(locationProvider)!!
+
+        return LatLng(lastKnownLocation.latitude, lastKnownLocation.longitude)
+    }
+
+    private fun onMyLocationButtonClick() {
+        when {
+            checkPermissions() -> googleMap?.moveCamera(
+                CameraUpdateFactory.newLatLngZoom(getMyLocation(), DEFAULT_ZOOM_LEVEL)
+            )
+            else -> Toast.makeText(applicationContext, "위치사용권한 설정에 동의해주세요", Toast.LENGTH_LONG)
+                .show()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
     }
 }
